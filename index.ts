6f352fc5ec5b867d3a775fdacb45069a60940366
Sven Röttering <svenroettering@googlemail.com>
2018-01-08 15:09:43 +0100

    Move mock.ts and component.ts into src directory. Introduce a dist directory for compiles ts files. Update .npmignore, .gitignore.

--- index.ts
+++ index.ts
@@ -1,19 +1,3 @@
-import { 
-  Component,
-  Directive,
-  Injectable,
-  NgModule,
-  Pipe
-} from '@angular/core';
-
-import { MockComponent } from './component';
-
-const Mock = (component: Component | Pipe | Directive | Injectable | NgModule): any => {
-  if (component instanceof Component) {
-    return MockComponent(component);
-  }
-  console.warn('Could not mock your type as it is currently not supported by ng-mock.');
-  return component;
-};
+import Mock from './src/mock';
 
 export default Mock;
