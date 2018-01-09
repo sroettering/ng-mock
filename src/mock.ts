564477639047a4a1f3a784df9ce973f7ace21d0c
Sven Röttering <svenroettering@googlemail.com>
2018-01-09 07:30:07 +0100

    Detect and mock Pipes

--- src/mock.ts
+++ src/mock.ts
@@ -1,13 +1,16 @@
 import { Component, Directive, Injectable, NgModule, Pipe } from '@angular/core';
 
 import { MockComponent } from './component';
+import { MockPipe } from './pipe';
 
-const Mock = (component: Component | Pipe | Directive | Injectable | NgModule): any => {
-    if (component instanceof Component) {
-        return MockComponent(component);
+const Mock = (element: Component | Pipe | Directive | Injectable | NgModule): any => {
+    if (element instanceof Component) {
+        return MockComponent(element);
+    } else if (element instanceof Pipe) {
+        return MockPipe(element);
     }
     console.warn('Could not mock your type as it is currently not supported by ng2-mock.');
-    return component;
+    return element;
 };
 
 export default Mock;
