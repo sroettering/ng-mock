440da9b0d49dbc74f57a0a31f3e86dcd7b3db95a
Sven Röttering <svenroettering@googlemail.com>
2018-01-22 21:36:05 +0100

    Support Angular 5

--- src/util/reflection.ts
+++ src/util/reflection.ts
@@ -2,11 +2,21 @@ import { Type } from '@angular/core';
 import 'reflect-metadata';
 
 export function annotations(clazz: any): any[] {
-    return Reflect.getOwnMetadata('annotations', clazz);
+    if (Reflect.hasOwnMetadata('annotations', clazz)) {
+        return Reflect.getOwnMetadata('annotations', clazz);
+    } else if (Object.getOwnPropertyDescriptor(clazz, '__annotations__')) {
+        return Object.getOwnPropertyDescriptor(clazz, '__annotations__').value;
+    }
+    return [];
 }
 
-export function propertyDecorators(clazz: any): any[] {
-    return Reflect.getMetadata('propMetadata', clazz);
+export function propertyDecorators(clazz: any): any {
+    if (Reflect.hasOwnMetadata('propMetadata', clazz)) {
+        return Reflect.getMetadata('propMetadata', clazz);
+    } else if (Object.getOwnPropertyDescriptor(clazz, '__prop__metadata__')) {
+        return Object.getOwnPropertyDescriptor(clazz, '__prop__metadata__').value;
+    }
+    return {};
 }
 
 export function propertiesWithDecoratorType(props: any = {}, decoratorType: any): string[] {
@@ -15,7 +25,7 @@ export function propertiesWithDecoratorType(props: any = {}, decoratorType: any)
         .filter(entry => entry[1]
         .some(decorator => decorator instanceof decoratorType))
         .map(entry => entry[0]);
-};
+}
 
 export function isOfType<T>(element: any, type: Type<T>): boolean {
     const annotationMetadata = annotations(element);
